5710b63643f9eae918c470c101d481b4a3f961ef
Jonathan Baldie <jon@jonbaldie.com>
2020-12-11 13:34:57 +0000

    work so far

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
+// Store the endpoint URL to save typing
+const endpoint = 'https://trainandpredict.com/api';
+
+// Instantiate the Axios object
 const axios = require('axios');
 
+// Write our object
 const tap = {
     logEvent: function (api_key, user_id, item_id) {
-        const response = axios.post('https://trainandpredict.com/api/log/' + user_id + '/' + item_id + '?api_key=' + api_key);
+        const response = axios.post(endpoint + '/log/' + user_id + '/' + item_id + '?api_key=' + api_key);
 
         return response;
     },
     logEventAsync: async function (api_key, user_id, item_id) {
-        const response = await axios.post('https://trainandpredict.com/api/log/' + user_id + '/' + item_id + '?api_key=' + api_key);
+        const response = await axios.post(endpoint + '/log/' + user_id + '/' + item_id + '?api_key=' + api_key);
 
         return response;
     },
     recommendForUser: function (api_key, user_id) {
-        const response = axios.get('https://trainandpredict.com/api/rec/user/' + user_id + '?api_key=' + api_key);
+        const response = axios.get(endpoint + '/rec/user/' + user_id + '?api_key=' + api_key);
 
         return response;
     },
     recommendForUserAsync: async function (api_key, user_id) {
-        const response = await axios.get('https://trainandpredict.com/api/rec/user/' + user_id + '?api_key=' + api_key);
+        const response = await axios.get(endpoint + '/rec/user/' + user_id + '?api_key=' + api_key);
 
         return response;
     },
     recommendForItem: function (api_key, item_id) {
-        const response = axios.get('https://trainandpredict.com/api/rec/item/' + item_id + '?api_key=' + api_key);
+        const response = axios.get(endpoint + '/rec/item/' + item_id + '?api_key=' + api_key);
 
         return response;
 
     },
     recommendForItemAsync: async function (api_key, item_id) {
-        const response = axios.get('https://trainandpredict.com/api/rec/item/' + item_id + '?api_key=' + api_key);
+        const response = axios.get(endpoint + '/rec/item/' + item_id + '?api_key=' + api_key);
 
         return response;
     },
