941e39c1fc8231afce64dad08c41f9adaecb0eee
Jonathan Baldie <jon@jonbaldie.com>
2020-12-11 13:32:33 +0000

    work so far

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const tap = function () {
+const axios = require('axios');
 
+const tap = {
+    logEvent: function (api_key, user_id, item_id) {
+        const response = axios.post('https://trainandpredict.com/api/log/' + user_id + '/' + item_id + '?api_key=' + api_key);
+
+        return response;
+    },
+    logEventAsync: async function (api_key, user_id, item_id) {
+        const response = await axios.post('https://trainandpredict.com/api/log/' + user_id + '/' + item_id + '?api_key=' + api_key);
+
+        return response;
+    },
+    recommendForUser: function (api_key, user_id) {
+        const response = axios.get('https://trainandpredict.com/api/rec/user/' + user_id + '?api_key=' + api_key);
+
+        return response;
+    },
+    recommendForUserAsync: async function (api_key, user_id) {
+        const response = await axios.get('https://trainandpredict.com/api/rec/user/' + user_id + '?api_key=' + api_key);
+
+        return response;
+    },
+    recommendForItem: function (api_key, item_id) {
+        const response = axios.get('https://trainandpredict.com/api/rec/item/' + item_id + '?api_key=' + api_key);
+
+        return response;
+
+    },
+    recommendForItemAsync: async function (api_key, item_id) {
+        const response = axios.get('https://trainandpredict.com/api/rec/item/' + item_id + '?api_key=' + api_key);
+
+        return response;
+    },
 };
 
 module.exports = tap;
